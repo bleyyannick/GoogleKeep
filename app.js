39f480f38cb28d7fc0e14e458c83fbef530915d0
Jeune Yannick <jeuneyannick@air-de-jeune.home>
2021-07-14 05:50:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+class App {
+    constructor() {
+        this.notes = []; 
+       
+        this.$placeholder = document;querySelector('#placeholder'); 
+        this.$form = document.querySelector("#form"); 
+        this.addEventListeners(); 
+        this.$noteTitle = document.querySelector('#note-title'); 
+        this.$formButtons = document.querySelector('#form-buttons'); 
+    }
+
+    addEventListeners() {
+        document.body.addEventListener('click', event => {
+            this.handleFormClick(event)
+        }); 
+        this.$form.addEventListener('submit', event => {
+            event.preventDefault(); 
+            const title = this.$noteTitle.value; 
+            const text = this.$noteText.value; 
+            const hasNote = title || text; 
+            if(hasNote) {
+                this.addNote({title, text}); 
+            }
+        })
+    }
+
+    
+
+    handleFormClick(event){
+        const isFormClicked = this.$form.contains(event.target); 
+        if(isFormClicked) {
+            this.openForm(); 
+        } else {
+            this.closeForm(); 
+        }
+    }
+
+    openForm() {
+        this.$form.classList.add("form-open"); 
+        this.$noteTitle.style.dsiplay = "block"; 
+        this.$formButtons.style.display = "block"; 
+    }
+
+    closeForm() {
+        this.$form.classList.remove("form-open"); 
+        this.$noteTitle.style.dsiplay = "none"; 
+        this.$formButtons.style.display = "none";
+    }
+
+    addNote(note) {
+        const newNote = {
+            title: note.title, 
+            text: note.text, 
+            color: white,
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1 
+
+        }; 
+        this.notes = [...this.notes, newNote]; 
+        this.displayNotes(); 
+    }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0; 
+        this.$placeholder.style.display =  hasNotes ? "none" : "flex"; 
+    }
+    
+}
+
+
+new App()
